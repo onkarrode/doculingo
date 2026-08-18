89a17ce3ece086a6916afde002d2c8ac13484501
onkarrode <onkarrode30@gmail.com>
2026-08-18 14:32:24 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const translationRoutes = require("./routes/translationRoutes");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -14,7 +14,7 @@ app.use(express.json());
 // Home route
 app.get("/", (req, res) => {
   res.json({
-    message: "DocTranslate backend is running!",
+    message: "DocuLingo backend is running!",
   });
 });
 
@@ -23,5 +23,5 @@ app.use("/api/translate", translationRoutes);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
